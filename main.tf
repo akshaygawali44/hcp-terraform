c568bf5ac0f3936bb4e837d848f2fd90cfc0c167
Akshay Gawali <95048781+akshaygawali44@users.noreply.github.com>
2026-04-01 00:01:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+
+}
+
+resource "aws_instance" "akshay" {
+  ami           = "ami-05d2d839d4f73aafb"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Application-Server"
+  }
+}
