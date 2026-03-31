c6ab7639410f26fd2750d5deba29fabee7d575ec
Akshay Gawali <95048781+akshaygawali44@users.noreply.github.com>
2026-04-01 00:12:58 +0530

    Change EC2 instance tag from 'Application-Server' to 'Web-Server'

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "akshay" {
   ami           = "ami-05d2d839d4f73aafb"
   instance_type = "t3.micro"
   tags = {
-    Name = "Application-Server"
+    Name = "Web-Server"
   }
 }
